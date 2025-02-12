3f78066967603b3d4faf1741c1bab43009f79c42
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-02-12 23:50:22 +0200

    feat: integrate Welcome and ListCast components into App component

--- hands-on-react/src/App.jsx
+++ hands-on-react/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import Support from "./components/Support";
+import Welcome from "./components/Welcome";
+import ListCast from "./components/ListCast";
 
 function App() {
   const name = "Stargazers";
@@ -8,15 +10,13 @@ function App() {
     <div className="container">
       <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
-        <h1>
-          Meet the <i>{name}</i>
-        </h1>
+        <Welcome name="StarGazers" />
         <p>
           Members of an <b>intergalactic alliance</b> paving the way for peace
           and benevolence among all species. They are known for their enthusiasm
           for science, for their love of fun, and their dedication to education.
         </p>
-
+        <ListCast />
         <Support />
       </hgroup>
     </div>
