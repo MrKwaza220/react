2d5b8d43a17076864000a7e3f46b8e1606175433
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-02-16 23:32:43 +0200

    feat: implement RegularList component to display a list of people

--- react-design-pattern/src/App.js
+++ react-design-pattern/src/App.js
@@ -1,19 +1,59 @@
+import { SmallPersonItem } from "./people/SmallPersonListItem";
+import { RegularList } from "./RegularList";
 import { SplitScreen } from "./SplitScreen";
 
-const LeftHandComponent = ({firstName}) => {
-  return <h1 style={{ backgroundColor: "green" }}>{firstName}</h1>;
-};
+const people = [
+  {
+    name: "John Doe",
+    age: 54,
+    hairColor: "brown",
+    hobbies: ["swimming", "bicycling", "video games"],
+  },
+  {
+    name: "Brenda Smith",
+    age: 33,
+    hairColor: "black",
+    hobbies: ["golf", "mathematics"],
+  },
+  {
+    name: 'Jane Garcia',
+    age: 27,
+    hairColor: 'blonde',
+    hobbies: ['biology', 'medicine', 'gymnastics'],
+  }
+];
 
-const RightHandComponent = ({secondName}) => {
-  return <h1 style={{ backgroundColor: "blue" }}>{secondName}</h1>;
-};
+const product = [
+  {
+    name: 'Flat-Screen TV',
+    price: 'R500',
+    description: 'Huge LCD Screen, a great deal',
+    rating: 4.5
+  },
+  {
+    name: 'Basketball',
+    price: 'R130',
+    description: 'Just like the pros use',
+    rating: 3.5
+  },
+  {
+    name: 'Running Shoes',
+    price: 'R100',
+    description: 'State-of-the-art technology for optimum',
+    rating: 4.2,
+  }
+
+];
+
+
 
 function App() {
   return (
-    <SplitScreen leftWeight={1} rightWeight={1}>
-      <LeftHandComponent firstName="Sakhumzi"/>
-      <RightHandComponent secondName="Kwaza"/>
-    </SplitScreen>
+    <RegularList 
+    items={people}
+    resourceName='person'
+    itemComponent={SmallPersonItem}
+    />
   );
 }
 
