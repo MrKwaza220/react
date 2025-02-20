6d56fdd04dc774d2c0d3e20cf6a5d3dfc4a38906
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-02-20 19:41:42 +0200

    feat: integrate ControlledModal into App component with toggle functionality

--- react-design-pattern/src/App.js
+++ react-design-pattern/src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import { ControlledForm } from "./components/controlledform/ControlledForm";
+import { Modal } from "./layout/Modal";
+import { ControlledModal } from "./components/controlledmodal/ControlledModal";
+import { UncontrolledModal } from "./components/uncontrolledmodal/UncontrolledModal";
 
 function App() {
+  const [shouldShowModal, setShouldShowModal] = useState(false);
   return (
       <>
-          <ControlledForm />
+          <ControlledModal
+          shouldShow={shouldShowModal}
+          onRequestClose={() => setShouldShowModal(false)}
+          >
+            <h1>Controlled Modal</h1>
+          </ControlledModal>
+          <button onClick={() => setShouldShowModal(!shouldShowModal)}>
+          
+            {shouldShowModal ? "Hide Modal" : "Show Modal"}
+          </button>
       </>
   );
 }
