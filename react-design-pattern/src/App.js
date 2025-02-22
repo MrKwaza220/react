ee2e08bf5aa67df3aef712fbe6946e189bc5bb77
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-02-22 20:06:51 +0200

    feat: replace modal implementation with UserInfor component for displaying user data

--- react-design-pattern/src/App.js
+++ react-design-pattern/src/App.js
@@ -1,24 +1,15 @@
-import { useState } from "react";
-import { ControlledForm } from "./components/controlledform/ControlledForm";
-import { Modal } from "./layout/Modal";
-import { ControlledModal } from "./components/controlledmodal/ControlledModal";
-import { UncontrolledModal } from "./components/uncontrolledmodal/UncontrolledModal";
+import { UserInfor } from "./customhooks/userinfor/UserInfor";
 
 function App() {
-  const [shouldShowModal, setShouldShowModal] = useState(false);
+
   return (
-      <>
-          <ControlledModal
-          shouldShow={shouldShowModal}
-          onRequestClose={() => setShouldShowModal(false)}
-          >
-            <h1>Controlled Modal</h1>
-          </ControlledModal>
-          <button onClick={() => setShouldShowModal(!shouldShowModal)}>
-          
-            {shouldShowModal ? "Hide Modal" : "Show Modal"}
-          </button>
-      </>
+    <>
+      <h1>Custom Hooks</h1>
+      <UserInfor userId= "124" />
+      <UserInfor userId= "456" />
+      <UserInfor userId= "789" />
+
+    </>
   );
 }
 
