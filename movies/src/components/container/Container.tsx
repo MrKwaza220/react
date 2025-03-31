6398b8870e1634c0c89da2f78f7b70be8b623949
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2025-03-31 07:11:24 +0200

    added changes

--- movies/src/components/container/Container.tsx
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-export const Container = styled.div`
-  padding: 10px 10rem;
-  height: 100vh;
-  background-color: #282c34;
-`;
--- react-redux-tutorials/src/App.js
+++ react-redux-tutorials/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreator } from './state/action-creators/index';
 import './App.css';
 
 function App() {
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
+  const {depositMoney, withdrawMoney} = bindActionCreators(actionCreator, dispatch)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {account}
+      <button onClick={() => depositMoney}>Deposit</button>
+      <button onClick={() => withdrawMoney}>withdraw</button>
     </div>
   );
 }
